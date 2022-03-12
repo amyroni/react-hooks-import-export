82f7d529dffdeac1327a5e6c910ce2b712e26012
Amy Shi <amyhshi92@gmail.com>
2022-03-11 21:23:35 -0500

    commit tree 39fc96025a0cdd706295c52b0e5869bea82c0e68

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RockyMountain from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(RockyMountain.trees);
+  RockyMountain.wildlife();
+  RockyMountain.elevation();
 
   return <h1>Colorado State Parks!</h1>;
+  
 }
+
+export default ColoradoStateParks;
